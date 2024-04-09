925aab6760edc64bf889b6b5fc2d5c0c096969da
Angelo Menel <angelomenel@hotmail.com>
2024-04-09 15:33:07 -0300

    Change i data-source

--- service-backend/src/data-source.ts
+++ service-backend/src/data-source.ts
@@ -20,7 +20,7 @@ import {
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.DB_HOST,
-    port: 54321,
+    port: parseInt(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
