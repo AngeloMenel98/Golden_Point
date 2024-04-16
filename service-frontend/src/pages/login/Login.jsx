3a9463edd45a424f644338e2963e66d8c3683000
santi2019 <santiagovietto5@hotmail.com>
2024-04-16 17:49:08 -0300

    primer modelo de vista

--- service-frontend/src/pages/login/Login.jsx
+++ service-frontend/src/pages/login/Login.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../images/golden_point_logo.png";
 import "./login.css";
 
@@ -15,22 +17,20 @@ const Login = ()=>{
                             <img className="l-logo" src={logo} alt="" />
                         </div>
                         <h1 class="l-title">Log in to your account</h1>
-                        <span class="l-span-su">Don’t have an account? Sign Up</span>
+                        <span class="l-span-da">Don’t have an account? <span class="l-span-su">Sign Up</span></span>
                         <div className="l-input-1">
                             <span className="l-span">Username</span>
-                            <input type="text" id="username" className="lInput"/>
+                            <input type="text" id="username" required="required"/>
                         </div>
                         <div className="l-input-2">
                             <span className="l-span">Password</span>
-                            <input type="password" id="password" className="lInput"/>
+                            <input type="password" id="password" required="required"/>
                             <span className="l-span-fp"> Forgot Password?</span>
                         </div>
-                        <button className="l-button">Login</button>
+                        <button className="l-button">Login <FontAwesomeIcon icon={faRightToBracket} className="l-icon" /></button>
                     </form>
                 </div>
-                <div className="l-image">
-                    <div className="image">
-                    </div>
+                <div className="l-wallpaper">
                 </div>
             </div>
         </div>
